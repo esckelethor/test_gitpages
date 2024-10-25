2fd1a5ddf5e7ec503b64091e89abc7eccc0e8a90
yeza <esckeleton@gmail.com>
2024-10-25 16:34:00 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+new Promise(function (resolve, reject) {
+		var xhr = new XMLHttpRequest();
+
+		xhr.onerror = function () {
+			return reject(new Error('XHR.onerror Exception...'));
+		}
+
+		xhr.onload = function() {
+			if (this.status == 200) {
+				return resolve(this.responseText);
+			} else {
+				return reject(new Error('XHR.onload Exception (Non 200 Response)... Refer to previous error'));
+			}
+		}
+
+		xhr.open('GET', './content/test.html', true);
+		xhr.send();
+	}).then(function(data) {
+    document.getElementById('change').innerHTML = data;
+  });
